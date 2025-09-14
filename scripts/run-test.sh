4eb6bb0def695dbab7acf8aac2546bfaf40c4f5b
pradr <pradr@WINAA5CG1075ZXT.localdomain>
2025-09-14 17:39:14 +0530

    Initial project structure with config, test, and run script

---
+++ scripts/run-test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "🚀 Running API Performance Test with JMeter..."
+
+docker run --rm -v $PWD/tests:/tests -v $PWD/reports:/reports \
+    justb4/jmeter -n -t /tests/sample_test.jmx -l /reports/result.jtl -e -o /reports/html
+
+echo " Test completed. Open reports/html/index.html to view results."
+
